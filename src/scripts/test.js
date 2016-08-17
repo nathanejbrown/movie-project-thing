badff8077e95a9346680b62ac0294a40f44e5edd
AustinMahan <amahan0424@gmail.com>
2016-08-17 14:42:22 -0600

    user input js

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -2,11 +2,11 @@ var Showtimes = require('showtimes');
 var api = new Showtimes(80241, {});
 
 
-  api.getMovies(function(err, movie){
-    if (err) {
-      throw err
-    }
-    movie.forEach(function(singleMov){
-      console.log(singleMov.name);
-    })
+api.getMovies(function(err, movie){
+  if (err) {
+    throw err
+  }
+  movie.forEach(function(singleMov){
+    console.log(singleMov.name);
   })
+})
