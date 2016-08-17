8d1744cf58c1d5745998033545919356bd71d189
Heathcliff <dennis.nathan@gmail.com>
2016-08-17 15:19:22 -0600

    added JS for google map to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,14 @@ $(document).ready(function() {
   console.log('sanity check');
   getTheDatas(80241, 'Suicide Squad')
 })
+
+function initialize(x,y) {
+  console.log(x + ' x - y ' + y);
+  var mapProp = {
+    center:new google.maps.LatLng(x,y),
+    zoom:15,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById('googleMap'),mapProp);
+}
+google.maps.event.addDomListener(window, 'load');
